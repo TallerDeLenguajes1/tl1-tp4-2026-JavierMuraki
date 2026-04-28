5375265e8728ee00fc5f8144c1c1d05ea22aeff4
Javier Muraki <javimuraki@gmail.com>
2026-04-27 23:26:43 -0300

    Transferencia de Tareas

--- To-Do.c
+++ To-Do.c
@@ -26,20 +26,25 @@ struct Nodo * BuscarNodo(struct Nodo * Start, int IdBuscado);
 struct Nodo * QuitarNodo(struct Nodo ** Start, int IdBuscado);
 void EliminarNodo(struct Nodo * VNodo);
 void EliminarListaNodo(struct Nodo * Start);
+void MostrarListaNodo(struct Nodo * Start);
+
 
 int main() {
     struct Nodo *TareasPendientes = NULL;
     struct Nodo *TareasRealizadas = NULL;
+    struct Nodo *NodoAux = NULL;
     char Buff[128];
     int Duracion = 0;
+    int ID = 0;
     int Continuar = 1;
 
     srand(time(NULL)); // Empezar con una semilla distinta para el generador de numeros aleatoreo
 
     while (Continuar) {
-        printf("Ingrese descripcion de tarea: ");
+        printf("Ingrese descripcion de Tarea: ");
         fflush(stdin); // Limpiar el buffer de la consola
         fgets(Buff, sizeof(Buff), stdin);
+        Buff[strcspn(Buff, "\n")] = 0; // Pasen como quitar el \n del final :pray:
 
         do {
             printf("Ingrese Duracion de tarea: ");
@@ -51,9 +56,42 @@ int main() {
 
         InsertarNodo(&TareasPendientes, CrearNodo(Buff, Duracion));
 
-        printf("Quieres ingresar otra tarea? (1 = SI / 0 = NO): ");
+        printf("Quieres ingresar otra Tarea? (1 = SI / 0 = NO): ");
+        fflush(stdin); // Limpiar el buffer de la consola
+        scanf("%d", &Continuar);
+        if (Continuar) printf("\n");
+    }
+
+    // Mostrar Tareas
+    printf("\n=== Tareas Pendientes ===\n");
+    MostrarListaNodo(TareasPendientes);
+    printf("\n=== Tareas Pendientes ===\n");
+    MostrarListaNodo(TareasRealizadas);
+
+    Continuar = 1;
+    while (Continuar) {
+        printf("\nIngrese ID de la tarea Realizada: ");
+        fflush(stdin); // Limpiar el buffer de la consola
+        scanf("%d", &ID);
+
+        NodoAux = QuitarNodo(&TareasPendientes, ID);
+        if (NodoAux) {
+            InsertarNodo(&TareasRealizadas, NodoAux);
+            printf("Se inserto la Tarea %d en la lista de Realizados\n", ID);
+        } else {
+            printf("No se encontro la Tarea con ID: %d\n", ID);
+        }
+
+        printf("Quieres marcar otra Tarea realizada? (1 = SI / 0 = NO): ");
+        fflush(stdin); // Limpiar el buffer de la consola
         scanf("%d", &Continuar);
     }
+
+    // Mostrar Tareas
+    printf("\n=== Tareas Pendientes ===\n");
+    MostrarListaNodo(TareasPendientes);
+    printf("\n=== Tareas Pendientes ===\n");
+    MostrarListaNodo(TareasRealizadas);
 
     // Liberar Memoria
     EliminarListaNodo(TareasPendientes);
@@ -139,5 +177,23 @@ void EliminarListaNodo(struct Nodo * Start) {
         NodoAux = NodoAux->Siguiente;
 
         EliminarNodo(NodoAnt);
+    }
+}
+
+void MostrarListaNodo(struct Nodo * Start) {
+    struct Nodo *NodoAux = Start;
+
+    if (NodoAux) {
+        while (NodoAux) {
+            printf("ID: %d\tDuracion: %d\tDesc: %s\n",
+                NodoAux->T.TareaID,
+                NodoAux->T.Duracion,
+                NodoAux->T.Descripcion
+            );
+
+            NodoAux = NodoAux->Siguiente;
+        }
+    } else {
+        printf("Lista vacia\n");
     }
 }
